4048200d248a4a6614360f9f907c329c5c6a068a
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-04 16:50:36 +0800

    Initial Commit

---
+++ Shared/OrderItApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderItApp.swift
+//  Shared
+//
+//  Created by Shane Leigh on 04/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct OrderItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
